c3fa198f1ca2e437ea2ff3b9cceee172ef7f3503
Preston Rollins <ptrollins@hotmail.com>
2020-07-24 11:13:22 -0400

    clean out unnecessary query

--- db/setup.sql
+++ db/setup.sql
@@ -34,31 +34,3 @@ CREATE TABLE answer_photos (
       REFERENCES answer (answer_id) MATCH SIMPLE
       ON DELETE CASCADE ON UPDATE NO ACTION
 );
-
-
-SELECT json_agg(results)
-      FROM (
-          SELECT q.question_id, q.question_body, q.question_date, q.asker_name, q.question_helpfulness, q.reported,
-          (
-          SELECT coalesce(json_object_agg(answer_id, answer), '{}'::JSON)
-          FROM (
-              SELECT a.answer_id, a.body, a.date, a.answer_name, a.helpfullness,
-                (
-                  SELECT array_to_json(coalesce(array_agg(photo), ARRAY[]::record[]))
-                  FROM (
-                    SELECT pap.id, pap.url
-                    FROM public.answer pa
-                    INNER JOIN public.answer_photos pap
-                    ON pa.answer_id = pap.answer_id
-                    WHERE pap.answer_id = a.answer_id
-                  ) photo
-                ) as photos
-              FROM public.answer a
-              INNER JOIN public.question pq
-              ON pq.question_id = a.question_id
-              WHERE a.question_id = q.question_id
-            ) answer
-          ) answers
-          FROM public.question q
-          WHERE q.product_id = $1
-      ) results
