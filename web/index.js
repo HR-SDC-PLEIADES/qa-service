77fe92388e8097a4b082fd92865813cd868d3574
Preston Rollins <ptrollins@hotmail.com>
2020-07-27 21:43:50 -0400

    add loader.io verification file

--- web/index.js
+++ web/index.js
@@ -13,6 +13,10 @@ app.use(bodyParser.json());
 
 app.use('/qa', router);
 
+app.get('/loaderio-e1fd998d1918962783aa240d06b6d9c3', (req, res) => {
+  res.sendFile('loaderio-verification.txt');
+});
+
 if (!module.parent) {
   app.listen(app.get('port'), () =>
     console.log('Project Greenfield listening on', app.get('port'))
