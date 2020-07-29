1f3814d7b3bab6457eab673481a1691685e61cd2
Preston Rollins <ptrollins@hotmail.com>
2020-07-29 10:59:09 -0400

    merge

--- web/index.js
+++ web/index.js
@@ -13,11 +13,7 @@ app.use(bodyParser.json());
 
 app.use('/qa', router);
 
-<<<<<<< HEAD
 app.get(`/${process.env.LOADERIO}`, (req, res) => {
-=======
-app.get('/loaderio-e1fd998d1918962783aa240d06b6d9c3', (req, res) => {
->>>>>>> 33d5cced7ebfde8f0dd03de7d0e9cfc492bb17d0
   res.sendFile('loaderio-verification.txt');
 });
 
